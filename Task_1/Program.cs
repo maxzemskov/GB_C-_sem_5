20ddbdb1392f219d1016e5def95366f117a0df65
mzemskov <mzemskov@dimax.digital>
2022-12-06 10:06:03 +0300

    task_1
    g
    
    git commit -m task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int sum = 0;
+
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(100, 999); 
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+    if ( array[i] % 2 == 0 )
+    sum = sum + array[i];
+
+Console.WriteLine(sum);
