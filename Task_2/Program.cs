c0c688496071f6cbd45551d2c4284f26d9d855a2
mzemskov <mzemskov@dimax.digital>
2022-12-07 20:19:49 +0300

    done

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int sum = 0;
+
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(); 
+
+
+for (int i = 0; i < array.Length; i++)
+{
+	if (i % 2 != 0)
+	sum = sum + array[i];
+}
+Console.WriteLine($"Массив[{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов с нечетными индексами = {sum}"); 
